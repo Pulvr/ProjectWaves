8ace2c11aa8d5674db8a62e14e4cb72d95559d0b
Pulvr <pulv3rtoastmann@gmail.com>
2024-05-21 13:32:26 +0200

    first movement from enemy, weird glitch issue with dragndrop atm

--- dragndrop.gd
+++ dragndrop.gd
@@ -6,21 +6,24 @@ var delay = 5
 
 func _physics_process(delta):
 	if is_dragging:
-		var new_position = get_global_mouse_position()
+		#var new_position = get_global_mouse_position()
 		
 		# Get viewport boundaries
-		var viewport = get_viewport_rect()
-		print(viewport.size)
-		var half_size = get_rect().size / 2
+		#var viewport = get_viewport_rect()
+		#print(viewport.size)
+		#var half_size = get_viewport_rect().size / 2
+		
 
 		# Clamp the position to keep the sprite within the screen bounds
 		# clamp(value,min,max)
-		new_position.x = clamp(new_position.x, half_size.x, viewport.size.x - half_size.x)
-		new_position.y = clamp(new_position.y, half_size.y, viewport.size.y - half_size.y)
+		#new_position.x = clamp(new_position.x, half_size.x, viewport.size.x - half_size.x)
+		#new_position.y = clamp(new_position.y, half_size.y, viewport.size.y - half_size.y)
+		#print(half_size.x)
+		#print(half_size.y)
 		
 		# Move the sprite smoothly to the clamped position
 		var tween = get_tree().create_tween()
-		tween.tween_property(self, "position", new_position, delay * delta)
+		tween.tween_property(self, "position", get_global_mouse_position() - mouse_offset, delay * delta)
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
@@ -28,6 +31,7 @@ func _input(event):
 			if get_rect().has_point(to_local(event.position)):
 				print("clicked on sprite")
 				is_dragging = true
+				mouse_offset = get_global_mouse_position() - global_position
 		else:
 			print("released button")
 			is_dragging = false
