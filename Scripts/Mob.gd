68d3b0a8161f39cb19ebc5d5333dc2c33a506453
Pulvr <pulv3rtoastmann@gmail.com>
2024-06-04 14:39:58 +0200

    tower finds enemies and shoots

--- Scripts/Mob.gd
+++ Scripts/Mob.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 # Gescwindigkeit des Mob, kann dank export ausserhalb des Skripts veränder werden
 @export var speed = 200
+@export var health = 10
 
 #Jeden Frame wird der ParentNodeProgress geprüft und der 
 #aktuelle Progress auf den jetzigen + speed*delta gesetzt. 
@@ -10,3 +11,5 @@ func _process(delta):
 	get_parent().set_progress(get_parent().get_progress() + speed*delta)
 	if get_parent().get_progress_ratio() == 1:
 		queue_free()
+	if health <= 0:
+		get_parent().get_parent().queue_free()
--- Scripts/coral_tower.gd
+++ Scripts/coral_tower.gd
@@ -8,12 +8,38 @@ var currTargets = [] 	# array mit allen currentTargets
 var curr 				# aktuelles Ziel
 
 
+func _process(delta):
+	#Kugeln die nicht mehr gebraucht werden löschen
+	if !is_instance_valid(curr):
+		for i in get_node("BulletContainer").get_child_count():
+			get_node("BulletContainer").get_child(i).queue_free()
+
 func _on_tower_body_entered(body):
 	if "Mob" in body.name:
 		var tempArray = []
 		currTargets = get_node("Tower").get_overlapping_bodies()
 		
-		print(currTargets)
+		for i in currTargets:
+			if "Mob" in i.name:
+				tempArray.append(i)
+		var currTarget = null
+		
+		for i in tempArray:
+			if currTarget == null:
+				currTarget = i.get_node("../")
+			else:
+				if i.get_parent().get_progress() > currTarget.get_progress():
+					currTarget = i.get_node("../")
+		
+		curr = currTarget
+		pathName = currTarget.get_parent().name
+		
+		var tempBullet  = Bullet.instantiate()
+		tempBullet.pathName = pathName
+		tempBullet.bulletDamage = bulletDamage
+		get_node("BulletContainer").call_deferred("add_child", tempBullet)
+		tempBullet.set_deferred("global_position", $Aim.global_position)
 
 func _on_tower_body_exited(body):
-	pass # Replace with function body.
+	currTargets = get_node("Tower").get_overlapping_bodies()
+	pass
