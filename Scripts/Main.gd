448bce0526123ba9ea2964484b9f16c503ebc332
LuK4s09 <lukasduerotin04@gmail.com>
2024-06-16 14:46:04 +0200

    jh

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -7,7 +7,7 @@ var max_hp = 100
 var current_hp = 100
 var money = 100
 
-@onready var hp_bar = $HP/HpBar
+@onready var hp_bar = $UI/HpBar
 
 func _ready():
 	update_hp_bar()
@@ -27,19 +27,19 @@ func heal(amount):
 #zum visuellen Updaten der Progress Bar
 func update_hp_bar():
 	hp_bar.value = current_hp
-<<<<<<< HEAD
+
 	
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
 		take_damage(10)
 	elif event.is_action_pressed("ui_cancel"):
 			heal(10)
-=======
 
-func _process(delta):
+
+func _process(_delta):
 	if current_hp <=0:
 		gameOver.emit()
 
 func _on_game_over():
 	get_tree().change_scene_to_file("res://Scenes/gameover_screen.tscn")
->>>>>>> main
+
